ae24ff036c27fd0492378fa2cd45390a822a1817
TheLeaner <leanercracker@gmail.com>
2018-07-14 23:35:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,9 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const request = require('request');
-let userData = request.get("http://www.robloxdataa.tk/skybot/userdata.json")
+const http = require('http-request');
+let userData = http.get("http://www.robloxdataa.tk/skybot/userdata.json")
+console.log(userData);
 client.on('ready', () => {
     client.user.setActivity(`IMPORTANT | -help`)
   })
@@ -19,7 +21,7 @@ client.on('message', message =>{
     if (!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {}
     if (!userData[sender.id + message.guild.id].money) userData[sender.id + message.guild.id].money = 100;
 
-    request(`http://www.robloxdataa.tk/skybot/post.php?content=${JSON.stringify(userData)}`), (err) => {
+    http.get(`http://www.robloxdataa.tk/skybot/post.php?content=${JSON.stringify(userData)}`), (err) => {
         if (err) console.log("POST ERROR | " + err)
     }
 
