2c2ced4ddba97f498e6d853e01c410ff5df9bf97
TheLeaner <leanercracker@gmail.com>
2018-11-07 18:24:48 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,12 +18,11 @@ client.on("ready", a => {
 function logEvery2Seconds(i) {
     setTimeout(() => {
         logEvery2Seconds(++i);
-    }, 2000)
+    }, 1500)
 }
 logEvery2Seconds(0);
 let i = 0;
 setInterval(() => {
-    console.log('Waiting...')
     request("http://ro-api.000webhostapp.com/ro-api/lastShout.txt", function (error, response, body) {
         lastShout = body
         });
@@ -34,9 +33,7 @@ setInterval(() => {
 
 
         if (lastShout == body["shout"]["body"]) {
-        console.log('Already last shout!')
         } else {
-        console.log('Sending group shout message!')
         client.guilds.get('509104083681148939').channels.get('509868150276358155').send("@here " + body["shout"]["body"] + " | https://www.roblox.com/My/Groups.aspx?gid=4511209")
         url = ("http://ro-api.000webhostapp.com/ro-api/setShout.php?contents="+body["shout"]["body"]);
         request(url, function (error, response, body) {
