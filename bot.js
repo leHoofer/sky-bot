d0d743830e07008d5ee57a098b17c4f08da50dfa
TheLeaner <leanercracker@gmail.com>
2018-11-16 19:04:19 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,13 @@ var sleep = require('sleep');
 
 //greyscale, invert, blur, flip
 
-
+function coroutine(f) {
+  var o = f(); // instantiate the coroutine
+  o.next(); // execute until the first yield
+  return function(x) {
+      o.next(x);
+  }
+}
 
 client.on("message", message => {
 
